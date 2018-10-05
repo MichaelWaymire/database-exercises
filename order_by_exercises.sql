3ad63053d70b70ddf3bc6c6db628575f44d2db82
MichaelWaymire <mike.waymire2@gmail.com>
2018-10-05 12:10:21 -0500

    finished ordere by and started limit exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,8 +1,9 @@
 use employees;
 
-select first_name from employees where first_name in ('Irena','Vidya') or first_name = 'Maya';
 
-select last_name from employees where last_name like 'E%';
+select first_name, last_name from employees where  first_name in ('Irena','Vidya', 'Maya') order by last_name;
+
+select emp_no, last_name from employees where last_name like 'E%' order by emp_no;
 
 select hire_date from employees where year(hire_date) like '1990%';
 
@@ -15,4 +16,4 @@ select last_name from employees where last_name like 'E%' or last_name like '%e'
 select last_name from employees where last_name like 'e%e';
 
 
-select hire_date, birth_date from employees where year(hire_date) like '1990%' and birth_date like '%-12-25';
+select first_name, last_name, hire_date, birth_date from employees where year(hire_date) like '1990%' and birth_date like '%-12-25' order by hire_date DESC;
